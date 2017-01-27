8d94fd59cbea656b5c87fb40c83a90dfdfa15a22
bilahari <bakkiraju@logitech.com>
2017-01-27 00:33:31 -0800

    hooked up fadeout to overlay window from self

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ var client = new net.Socket();
 
 //$(".app1").bind('animationend', function() { remote.getCurrentWindow().hide(); } );
 
-ipcRenderer.on('done',(event, arg) =>{
-  console.log("changing the class");
-  var elem = $(".app").attr('class','app1');
-  var x = document.querySelector(".app1");
-
-  // Code for Chrome, Safari and Opera
-  x.addEventListener("animationend", (e)=>{
-    remote.getCurrentWindow().hide();
-  }, false);
-
-});
+// ipcRenderer.on('done',(event, arg) =>{
+//   console.log("changing the class");
+//   var elem = $(".app").attr('class','app1');
+//   var x = document.querySelector(".app1");
+//
+//   // Code for Chrome, Safari and Opera
+//   x.addEventListener("animationend", (e)=>{
+//     remote.getCurrentWindow().hide();
+//   }, false);
+// });
 
 connectToUIEngine = ()=> {
   client.connect(PORT, HOST, function() {
@@ -28,6 +27,47 @@ connectToUIEngine = ()=> {
 };
 
 showOverlay1 = ()=> {
+    console.log("Sending message to show overlay type1");
+
+    jsonInput = {
+     RenderingOptions: {
+            RenderType: "Electron",
+            size: {
+              height: 300,
+              width : 400
+            },
+            position: {
+                top: 278,
+                left: 520
+            },
+            ToolOptions: [
+                {
+                   optName: "hg",
+                   optSymbolType: "icon",
+                   optSymbolValue:"icon-hourglass",
+                   optValue:10
+                },
+                {
+                   optName: "Publish",
+                   optSymbolType: "icon",
+                   optSymbolValue:"icon-publish",
+                   optValue:15
+                },
+                {
+                   optName: "ld",
+                   optSymbolType: "icon",
+                   optSymbolValue:"icon-light-down",
+                   optValue:11
+                }
+              ],
+            RenderFile:"prefs.html"
+    }
+  };
+
+   client.write(JSON.stringify(jsonInput));
+};
+
+showOverlay2 = ()=> {
     console.log("Sending message to show overlay type1");
 
     jsonInput = {
@@ -83,7 +123,15 @@ closeOverlay1 = ()=> {
 
 fadeOutOverlay1 = () => {
   console.log("Sending message to fade out the overlay");
-  ipcRenderer.send('make-transparent');
+  jsonInput = {
+   RenderingOptions: {
+          RenderType: "Electron",
+          Command:"fade-out"
+  }
+};
+
+ client.write(JSON.stringify(jsonInput));
+  //ipcRenderer.send('make-transparent');
 }
 
 // data is what the server sent to this socket
